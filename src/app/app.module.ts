e3ba4bb42c87b38bee6966b2dd688da52d3fa9f1
ankursharma02 <ankurs083@outlook.com>
2018-12-01 17:20:38 +0530

    com

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SearchPage } from '../pages/search/search';
+import { MapPage } from '../pages/map/map';
+import { RequisitionPage } from '../pages/requisition/requisition';
+import { TicketsPage } from '../pages/tickets/tickets';
+import { SignOutPage } from '../pages/sign-out/sign-out';
+import { SettingPage } from '../pages/setting/setting';
+import { ManualPage } from '../pages/manual/manual';
+import { CollaborationPage } from '../pages/collaboration/collaboration';
+import { ELogPage } from '../pages/e-log/e-log';
+import { CompanyValidationDemoPage } from '../pages/company-validation-demo/company-validation-demo';
+import { UserValidationDemoPage } from '../pages/user-validation-demo/user-validation-demo';
+import { PasswordValidationDemoPage } from '../pages/password-validation-demo/password-validation-demo';
+import { LoadingPage } from '../pages/loading/loading';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    SearchPage,
+    MapPage,
+    RequisitionPage,
+    TicketsPage,
+    SignOutPage,
+    SettingPage,
+    ManualPage,
+    CollaborationPage,ELogPage,
+    CompanyValidationDemoPage,
+    UserValidationDemoPage,
+    PasswordValidationDemoPage,
+    LoadingPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,SearchPage,MapPage,RequisitionPage,TicketsPage,SignOutPage,SettingPage,
+    ManualPage,CollaborationPage,ELogPage,CompanyValidationDemoPage,UserValidationDemoPage, PasswordValidationDemoPage,
+    LoadingPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
